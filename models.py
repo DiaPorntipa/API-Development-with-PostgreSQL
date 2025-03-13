2f473efe6bdb7932edfe3a3700de5a0364fab42f
porntipapoonpolsub <u7618313@anu.edu.au>
2025-03-13 18:30:38 +1100

    Created data models in model.py

---
+++ models.py
@@ -0,0 +1,19 @@
+# Database Models
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password_hash = Column(String)
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User")
